cbab45ff9439b33f105a6f433eb2151885230863
Avinash Singh <143834604+Avinash0011@users.noreply.github.com>
2026-05-04 19:23:41 +0530

    Update script.js

--- part-1/script.js
+++ part-1/script.js
@@ -1,7 +1,10 @@
-// Variables and Declaration 
+/// Variables 
 var a;              // variables and let esliye use krte h jese baad m jaa k use kr ske us value ko
 var a=12;
 let a;            
 let a=12;
 
 const a=12;         // constant fix rheta h change nhi hota lkn usse update kr skte h
+
+
+///  Declarations and initialization 
