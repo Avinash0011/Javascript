cc121247c41cd856cff7648cbe8216d11e96a222
Avinash Singh <143834604+Avinash0011@users.noreply.github.com>
2026-05-04 19:17:18 +0530

    Implement variable declarations in script.js
    Add variable declarations using var, let, and const.

---
+++ part-1/script.js
@@ -0,0 +1,7 @@
+// Variables and Declaration 
+var a;              // variables and let esliye use krte h jese baad m jaa k use kr ske us value ko
+var a=12;
+let a;            
+let a=12;
+
+const a=12;         // constant fix rheta h change nhi hota lkn usse update kr skte h
